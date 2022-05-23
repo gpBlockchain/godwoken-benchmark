8b0a55ccaa10285328e75972625f88da0ca8d245
linguopeng <linguopeng@hyperchain.cn>
2022-05-23 21:02:32 +0800

    fix register

--- index_account.js
+++ index_account.js
@@ -1,13 +1,11 @@
-const fs = require('fs').promises
 const Web3 = require('web3')
-
-const { register_account, deployCkbProxyContact, get_benchmark_accounts_by_path } = require('./src/account_register')
+const { register_account, deployCkbProxyContact, get_benchmark_accounts_by_path } = require('./src/account_register.js');
 
 (async () => {
 	const ACCOUNT_PRIVATE_KEY = '0xdd50cac37ec6dd12539a968c1a2cbedda75bd8724f7bcad486548eaabb87fc8b'
 	const rpc_url = 'http://localhost:8024'
 	const TRANSFER_BALANCE = 1000000000000000000n
-	const register_num = 50
+	const register_num = 1000
 	const keystore_path = './keystore/account.private'
 
 	const web3 = new Web3(new Web3.providers.HttpProvider(rpc_url))
@@ -30,8 +28,7 @@ const { register_account, deployCkbProxyContact, get_benchmark_accounts_by_path 
 				{
 					from: old_account.address,
 					gas: 6000000
-				}
-			)
+				})
 			tokenBalanceOnLayer2 = await ckbErc20Contract.methods.balanceOf(account.address).call()
 		}
 		console.log('account:', account.address, ' balanceOf:', tokenBalanceOnLayer2)
